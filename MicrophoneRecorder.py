75e2853c649476e68dbfa477f4ccd48c01d7034d
HARSH <160406269+7-FLASH-7@users.noreply.github.com>
2025-05-17 16:28:26 +0100

    This one will check if user is speaking in microphone and writes whatever it is peaking

--- MicrophoneRecorder.py
+++ MicrophoneRecorder.py
@@ -7,7 +7,6 @@ recognizer = speech_recognition.Recognizer()
 while True:
 
     try:
-
         with speech_recognition.Microphone() as mic:
 
             recognizer.adjust_for_ambient_noise(mic, duration=0.2)
