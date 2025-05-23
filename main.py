e4f24b4d66d5509b7b369d4cb558c235ea18097c
HARSH <160406269+7-FLASH-7@users.noreply.github.com>
2025-05-24 00:10:36 +0100

    updated the code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ActivationWord import detect_activation_word
 from SpeechRecognition import recognize_speech
 
-if detect_wake_word():
+if detect_activation_word():
     command = recognize_speech()
     if command:
         print(f"Command received: {command}")
